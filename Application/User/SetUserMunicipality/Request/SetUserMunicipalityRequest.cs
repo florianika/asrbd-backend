0e3ed64a2975d4968d0c93720b513a14c321f88c
Florian <florianika@gmail.com>
2025-03-28 23:27:26 +0100

    Better validation of set municipality request

--- Application/User/SetUserMunicipality/Request/SetUserMunicipalityRequest.cs
+++ Application/User/SetUserMunicipality/Request/SetUserMunicipalityRequest.cs
@@ -13,7 +13,9 @@ public class SetUserMunicipalityRequestValidator : AbstractValidator<SetUserMuni
 {
     public SetUserMunicipalityRequestValidator() 
     {
-        RuleFor(sum => sum.MunicipalityCode).NotEmpty().NotNull();
+        RuleFor(sum => sum.MunicipalityCode).NotEmpty().NotNull().MinimumLength(1).MaximumLength(2)
+            .Must(code => int.TryParse(code, out var result) && result is >= 1 and <= 61)
+            .WithMessage("Invalid Municipality Code");
         RuleFor(sum => sum.UserId).NotEmpty().NotNull();
     }
     
